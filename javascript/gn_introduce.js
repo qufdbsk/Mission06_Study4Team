8894c62963473063549afdf43032586a97e0b788
qkr5318 <qkr5318@gmail.com>
2022-12-08 21:03:27 +0900

    re commit

--- javascript/gn_introduce.js
+++ javascript/gn_introduce.js
@@ -1,4 +1,4 @@
-
+  // 스크린 990보다 작을때 on 클래스 생기면서 display-non
   $(document).ready(function(){
     if($(this).width() < 990){
       console.log(this);
@@ -14,6 +14,7 @@
         $(".navbar-form > div").removeClass("on");
     }
     });
+    // alert 아이디 중복 버튼 추후 아이디수 및 검수 수정예정
     $(".id_overlap button").on("click", function(){
      
       alert("사용할 수 있는 ID 입니다.")
